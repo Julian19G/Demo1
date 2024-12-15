8e4aace0aad91a7e7177c2009be1fa0a0be1aadf
Usuario <julian19guerra@gmail.com@DESKTOP-FM3NDOL>
2024-12-15 17:31:22 -0500

    Console 1

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Write("Hello, Writer!");
+
+            FiggleWriter MyFiggle = new FiggleWriter();
+            MyFiggle.Write("Hello, FiggleWriter!!!");
+        }
+    }
+}
